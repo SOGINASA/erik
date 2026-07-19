018c6817ec6ba326566053ed29002fdaee7c34a1
boom_boy322 <evanifrimov@gmail.com>
2026-07-19 16:59:32 +0500

    more docker ports

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,4 @@
 set -e
 
 echo "[entrypoint] Starting gunicorn..."
-exec gunicorn --preload -w 4 --threads 10 -b 0.0.0.0:5000 --timeout 120 app:app
+exec gunicorn --preload -w 4 --threads 10 -b 0.0.0.0:6752 --timeout 120 app:app
